1abbf010ff983e42575c423eb5309fc4941b3c18
创圣大天使Evol <296662402@qq.com>
2024-09-03 14:59:43 +0800

    import tailwindcss use tailwindcss plugin in postcss.config.js @import url() in index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import { AppsProvider } from './hooks/index'
-import './style/index.less'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
